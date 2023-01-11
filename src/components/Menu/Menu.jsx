6a36019ced05fe281b87a42585ae40943d79fad1
Dmitry <dimaruzaev2@gmai.com>
2023-01-11 16:03:56 +0300

    change Menu - v1.0

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { Component, Fragment } from "react";
 import { CSSTransition } from "react-transition-group";
 
 import "./Menu.scss";
@@ -16,7 +16,7 @@ const menuDB = {
 		{
 			name: "Дом",
 			icon: house,
-			contentContent: [
+			options: [
 				{
 					name: "Сад",
 					icon: garden,
@@ -49,7 +49,8 @@ const menuDB = {
 	],
 };
 
-const sizeIcon = 44; // dafault 44px
+const sizeIcon = 44; // default 44px
+const sizeOption = 18; // default 18px
 
 export default class Menu extends Component {
 	constructor() {
@@ -75,41 +76,61 @@ export default class Menu extends Component {
 						<ul className="menu__list">
 							<ul className="menu__select">
 								{
-									menuDB.innerContentItem.map(({ name, icon }, index) => {
+									menuDB.innerContentItem.map(({ name, icon, options }, index) => {
 										let numGroup = 0;
 										let active = isActive === String(numGroup) + String(index);
 
 										return (
-											<CSSTransition in={active} timeout={0} key={name}>
+											<Fragment>
+												<CSSTransition in={active} timeout={100}>
+													<ul className="menu__options">
+														{
+															options.map(({ name, icon }) => {
+																return (
+																	<li className="menu__option" key={name} tabIndex={0}>
+																		<img src={icon} alt={name} className="menu__option-icon" width={sizeOption} height={sizeOption}/>
+																		<span className="menu__option-title">{name}</span>
+																	</li>
+																)
+															})
+														}
+													</ul>
+												</CSSTransition>
+
+												<CSSTransition in={active} timeout={0} key={name}>
+													<li onClick={() => this.handleActive(numGroup, index)}
+														className={"menu__select-item" + (active ? " menu__select-item_active" : "")}
+														tabIndex={0}>
+														<img src={icon} alt={name} className="menu__select-icon" width={sizeIcon} height={sizeIcon}/>
+														<span className="menu__select-title">{name}</span>
+														<button className="menu__select-exit" onClick={() => {numGroup = -1}} />
+													</li>
+												</CSSTransition>
+											</Fragment>
+										)
+									})
+								}
+							</ul>
+						
+							<CSSTransition in={isActive === "00"} timeout={200}>
+								<ul className="menu__link">
+									{
+										menuDB.linkItem.map(({ name, icon }, index) => {
+											const numGroup = 1;
+											let active = isActive === String(numGroup) + String(index);
+
+											return (
 												<li onClick={() => this.handleActive(numGroup, index)}
-													className={"menu__select-item" + (active ? " menu__select-item_active" : "")}
-													tabIndex="0">
-													<img src={icon} alt={name} className="menu__select-icon" width={sizeIcon} height={sizeIcon}/>
-													<button className="menu__select-exit" onClick={() => {numGroup = -1}} />
+													className={"menu__link-item" + (active ? " menu__link-item_active" : " menu__link-item_hover")}
+													key={name}
+													tabIndex={0}>
+													<img src={icon} alt={name} className="menu__link-icon" width={sizeIcon} height={sizeIcon}/>
 												</li>
-											</CSSTransition>
-										)
-									})
-								}
-							</ul>
-
-							<ul className="menu__link">
-								{
-									menuDB.linkItem.map(({ name, icon }, index) => {
-										const numGroup = 1;
-										let active = isActive === String(numGroup) + String(index);
-
-										return (
-											<li onClick={() => this.handleActive(numGroup, index)}
-												className={"menu__link-item" + (active ? " menu__link-item_active" : " menu__link-item_hover")}
-												key={name}
-												tabIndex="0">
-												<img src={icon} alt={name} className="menu__link-icon" width={sizeIcon} height={sizeIcon}/>
-											</li>
-										)
-									})
-								}
-							</ul>
+											)
+										})
+									}
+								</ul>
+							</CSSTransition>
 						</ul>
 					</nav>
 				</div>
