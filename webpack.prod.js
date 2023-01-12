4909013d2a10c80eb68d4746fa50ae7340e19681
Dmitry <dimaruzaev2@gmai.com>
2023-01-12 13:35:18 +0300

    remove /public/ and add deploy v0.1

--- webpack.prod.js
+++ webpack.prod.js
@@ -18,7 +18,7 @@ module.exports = {
 		}
 	},
 	output: {
-		path: path.resolve(__dirname, 'build'),
+		path: path.resolve(__dirname, 'dist'),
 		clean: true
 	},
 	optimization: {
@@ -75,7 +75,7 @@ module.exports = {
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
-			template: '../public/index.html',
+			template: './index.html',
 			filename: 'index.html'
 		}),
 		new MiniCssExtractPlugin({
